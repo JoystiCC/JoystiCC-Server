ad68cee6086a2dabcf780fc0e1bdbc1ee894ac56
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-05-21 14:40:15 -0400

    Let Players render JSON

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,6 +7,7 @@ class PlayersController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @players }
+      format.json  { render :json => @players }
     end
   end
 
@@ -18,6 +19,7 @@ class PlayersController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @player }
+      format.json  { render :json => @player }
     end
   end
 
@@ -29,6 +31,7 @@ class PlayersController < ApplicationController
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @player }
+      format.json  { render :json => @player }
     end
   end
 
@@ -46,9 +49,11 @@ class PlayersController < ApplicationController
       if @player.save
         format.html { redirect_to(@player, :notice => 'Player was successfully created.') }
         format.xml  { render :xml => @player, :status => :created, :location => @player }
+        format.json  { render :json => @player, :status => :created, :location => @player }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @player.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @player.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -62,9 +67,11 @@ class PlayersController < ApplicationController
       if @player.update_attributes(params[:player])
         format.html { redirect_to(@player, :notice => 'Player was successfully updated.') }
         format.xml  { head :ok }
+        format.json { head :ok }
       else
         format.html { render :action => "edit" }
         format.xml  { render :xml => @player.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @player.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -78,6 +85,7 @@ class PlayersController < ApplicationController
     respond_to do |format|
       format.html { redirect_to(players_url) }
       format.xml  { head :ok }
+      format.json { head :ok }
     end
   end
 end
