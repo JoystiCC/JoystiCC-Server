6e76be0703711db86cf78ff01be57725904d7842
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-05-21 14:00:42 -0400

    Created Game scaffold

---
+++ db/migrate/20110521180037_create_games.rb
@@ -0,0 +1,16 @@
+class CreateGames < ActiveRecord::Migration
+  def self.up
+    create_table :games do |t|
+      t.string :name
+      t.int :owner_id
+      t.int :start_time
+      t.int :end_time
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :games
+  end
+end
