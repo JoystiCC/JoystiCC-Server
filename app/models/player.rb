c2a3e53d254a4952a666b2a5a4754fc9b734b3b6
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-05-21 15:12:42 -0400

    Players don't require teams/games

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,5 @@ class Player < ActiveRecord::Base
 	belongs_to :team
 	has_one :game, :through => :team
 
-	validates_presence_of :username, :team, :game
-	validates_associated :team, :game
+	validates_presence_of :username
 end
