42e0449e0e220925f3fa6e7e38dd4d3376d4d742
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-05-21 13:43:05 -0400

    Initialized rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveGame::Application.config.secret_token = '3aaa1236d5b54d46230a3b57db6c455041c17a9b6a5cfd78948a6871f5610e56578e96f91d06897ee3a295e39bd98cfdf6c13fb794bdd4a556267df4e1dcb081'
