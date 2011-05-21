695381aebc7575c07b11e3c09b307f0836254851
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-05-21 14:41:38 -0400

    Let Teams render JSON

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,6 +7,7 @@ class TeamsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @teams }
+      format.json  { render :json => @teams }
     end
   end
 
@@ -18,6 +19,7 @@ class TeamsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @team }
+      format.json  { render :json => @team }
     end
   end
 
@@ -29,6 +31,7 @@ class TeamsController < ApplicationController
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @team }
+      format.json  { render :json => @team }
     end
   end
 
@@ -46,9 +49,11 @@ class TeamsController < ApplicationController
       if @team.save
         format.html { redirect_to(@team, :notice => 'Team was successfully created.') }
         format.xml  { render :xml => @team, :status => :created, :location => @team }
+        format.json  { render :json => @team, :status => :created, :location => @team }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @team.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @team.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -62,9 +67,11 @@ class TeamsController < ApplicationController
       if @team.update_attributes(params[:team])
         format.html { redirect_to(@team, :notice => 'Team was successfully updated.') }
         format.xml  { head :ok }
+        format.json  { head :ok }
       else
         format.html { render :action => "edit" }
         format.xml  { render :xml => @team.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @team.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -78,6 +85,7 @@ class TeamsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to(teams_url) }
       format.xml  { head :ok }
+      format.json { head :ok }
     end
   end
 end
