644e2617b9311e10c4f988fa7098a50d6b8999a1
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-05-21 15:22:40 -0400

    Fixed relation between game and owner (player)

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
 	has_many :teams
-	has_one :owner, :class_name => "Player"
+	belongs_to :owner, :class_name => "Player"
 
 	validates_presence_of :password, :name
 	validates_associated :owner, :teams
