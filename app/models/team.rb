f5ee12b48f8c5a996b122f541a2f9c2181878a2a
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-05-21 21:25:22 -0400

    Don't limit team size

--- app/models/team.rb
+++ app/models/team.rb
@@ -8,13 +8,6 @@ class Team < ActiveRecord::Base
 	validates_presence_of :name
 
     validate :ensure_controller_exists
-    validate :ensure_team_limit
-
-    #Teams must have at least 1 player and no more than 4
-    #We check for >= 2 and <= 5 because the controller counts as a player, and we don't want to include them
-    def ensure_team_limit
-    	errors.add(self, 'cannot have more than 4 or less than 1 player') unless self.players.count >= 2 and self.players.count <= 5
-    end
 
 	def ensure_controller_exists
     	errors.add(:controller,'must exist') unless self.controller
