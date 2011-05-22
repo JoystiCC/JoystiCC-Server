dba1c2877989d195690c3fc2eeb969879f538691
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-05-21 21:28:21 -0400

    Ensure game exists

--- app/models/team.rb
+++ app/models/team.rb
@@ -8,9 +8,14 @@ class Team < ActiveRecord::Base
 	validates_presence_of :name
 
     validate :ensure_controller_exists
+    validate :ensure_game_exists
 
 	def ensure_controller_exists
     	errors.add(:controller,'must exist') unless self.controller
+  	end
+
+	def ensure_game_exists
+    	errors.add(:game,'must exist') unless self.game
   	end
 
   	def to_json(options={})
